9e142e5546b233cb9c19dca010d3fdd257e9fe7f
EdgarMachado <42387128+EdgarMachado@users.noreply.github.com>
2022-02-23 07:31:03 -0300

    first deploy

---
+++ web/src/pages/ArticlePage/ArticlePage.js
@@ -0,0 +1,13 @@
+import ArticleCell from 'src/components/ArticleCell'
+import { MetaTags } from '@redwoodjs/web'
+
+const ArticlePage = ({ id }) => {
+  return (
+    <>
+      <MetaTags title="Article" description="Article page" />
+      <ArticleCell id={id} />
+    </>
+  )
+}
+
+export default ArticlePage
